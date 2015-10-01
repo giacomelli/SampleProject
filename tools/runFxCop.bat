e26b240a698891428cd40eba4a038961cdb9fa61
unknown <giacomelli@CG-003130.cwinet.local>
2015-10-01 13:11:37 -0300

    Added FxCop HTML report.

--- tools/runFxCop.bat
+++ tools/runFxCop.bat
@@ -1,7 +1,7 @@
 @echo ------------------------------------------
 @echo Running FxCop...
 @echo ------------------------------------------
-@"C:\Program Files (x86)\Microsoft Fxcop 10.0\FxCopCmd.exe" /project:"..\SRC\SampleProject.FxCop" /out:fxcop-report.xml
+@"C:\Program Files (x86)\Microsoft Fxcop 10.0\FxCopCmd.exe" /project:"..\SRC\SampleProject.FxCop" /out:fxcop-report.html /oXsl:FxCopRichConsoleOutput.xsl /applyoutXsl
 @echo fxcop-report.xml file created.
 
 @echo Done!
