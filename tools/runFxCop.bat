7f369e3eb8d095a9b874b1a1d1c20e09c17eae01
unknown <giacomelli@CG-003130.cwinet.local>
2015-10-01 15:52:14 -0300

    Testing FxCop badge

--- tools/runFxCop.bat
+++ tools/runFxCop.bat
@@ -1,8 +1,11 @@
 @echo ------------------------------------------
 @echo Running FxCop...
 @echo ------------------------------------------
-@"C:\Program Files (x86)\Microsoft Fxcop 10.0\FxCopCmd.exe" /project:"..\SRC\SampleProject.FxCop" /out:fxcop-report.html /oXsl:FxCopReport.xsl /applyoutXsl
-@echo fxcop-report.xml file created.
+rem @"C:\Program Files (x86)\Microsoft Fxcop 10.0\FxCopCmd.exe" /project:"..\SRC\SampleProject.FxCop" /out:fxcop-report.html /oXsl:FxCopReport.xsl /applyoutXsl
+@"C:\Program Files (x86)\Microsoft Fxcop 10.0\FxCopCmd.exe" /project:"..\SRC\SampleProject.FxCop" /out:fxcop-report.xml
+@echo fxcop-report.html file created.
+
+DotBadge -b "FxCop:fxcop-report.xml" -output "FxCop-badge.svg"
 
 @echo Done!
 @pause
