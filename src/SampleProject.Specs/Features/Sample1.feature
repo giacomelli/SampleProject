1c803f17cefcfa95c063dfd579f95b8a7ca68df0
unknown <giacomelli@CG-003130.cwinet.local>
2015-10-05 07:45:53 -0300

    Added SpecFlow sample.

---
+++ src/SampleProject.Specs/Features/Sample1.feature
@@ -0,0 +1,36 @@
+﻿#language: pt-br
+Funcionalidade: Exemplo 1
+	COMO usuário
+	QUERO acessar a página de exemplo
+	PARA demonstrar o uso do SpecFlow
+	 
+Contexto:          
+	Quando acesso a página principal
+
+Cenário: Páginal principal
+	Então deve exibir o texto 'ASP.NET is a free web framework'
+
+	Quando clico no link 'About'
+	Então deve exibir o texto 'Your application description page'
+
+	Quando clico no link 'Contact'
+	Então deve exibir o texto 'Your contact page'
+
+	Quando clico no link 'Register'
+	Então deve exibir o texto 'Create a new account'
+	Quando clico no botão 'Register new'
+	Então deve exibir o texto 'O campo Email é obrigatório'
+	E deve exibir o texto 'O campo Password é obrigatório'
+	
+	Quando digito 'test@sampleproject.com' no campo 'Email'
+	Quando clico no botão 'Register new'
+	Então não deve exibir o texto 'O campo Email é obrigatório'
+	E deve exibir o texto 'O campo Password é obrigatório'
+	
+	Quando digito 'abc' no campo 'Password'
+	Quando clico no botão 'Register new'
+	Então não deve exibir o texto 'O campo Email é obrigatório'
+	E não deve exibir o texto 'O campo Password é obrigatório'
+	E deve exibir o texto 'The Password must be at least 6 characters long'
+	E deve exibir o texto 'The password and confirmation password do not match'
+
---
+++ src/SampleProject.Specs/Integrations/SpecFlowHeperConfig.cs
@@ -0,0 +1,44 @@
+﻿using HelperSharp;
+using SpecFlowHelper.Configuration;
+using SpecFlowHelper.Integrations;
+using SpecFlowHelper.Integrations.Browsers;
+using SpecFlowHelper.Logging;
+using SampleProject.Specs.Integrations.Environments;
+using SpecFlowHelper.Steps;
+using SpecFlowHelper.Steps.Strategies;
+using SpecFlowHelper.Steps.Strategies.jQuery;
+using TechTalk.SpecFlow;
+using TestSharp;
+
+namespace SampleProject.Specs.Integrations
+{
+    [Binding]
+    public class SpecFlowHeperConfig
+    {
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            AppConfig.Environment = new SampleContinuousIntegrationEnvironment();
+            AppConfig.BrowserKind = BrowserKind.Chrome;
+            AppConfig.BrowserDriverFolder = VSProjectHelper.GetProjectFolderPath("SampleProject.Specs") + @"\Drivers";
+            AppConfig.Configuration = "Debug";
+
+            AppConfig.InitializeDBEnabled = false;
+
+            AppConfig.WebApiEnabled = true;
+            AppConfig.WebApiProjectFolderName = "SampleProject.WebApi";
+            AppConfig.WebApiPort = 8001;
+
+            AppConfig.WebAppProjectFolderName = "SampleProject.WebApp";
+            AppConfig.WebAppPort = 8002;
+            AppConfig.WebAppBaseUrl = "http://localhost:8002";
+
+            AppConfig.JobsEnabled = true;
+            AppConfig.JobsLogFileName = "SampleProject.BackgroundWorkerApp-log.txt";
+            AppConfig.JobsProcessName = "SampleProject.BackgroundWorkerApp";
+            AppConfig.JobsProjectFolderName = "SampleProject.BackgroundWorkerApp";
+      
+            StrategyFactory.Register<IComboboxStepsStrategy, ComboboxSteps>(new jQueryComboboxStepsStrategy());
+        }
+    }
+}
