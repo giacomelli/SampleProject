3b5ba70f64c01b62742afebb9b81f4c37d171add
unknown <giacomelli@CG-003130.cwinet.local>
2015-10-05 09:38:28 -0300

    Using latest exit code to generate SpecFlow badge.

--- tools/runSpecFlow.bat
+++ tools/runSpecFlow.bat
@@ -3,6 +3,7 @@
 @echo ------------------------------------------
 
 vstest.console.exe "..\src\SampleProject.Specs\bin\DEV\SampleProject.Specs.dll"       
+BadgesSharp\BadgesSharpCmd.exe "https://github.com/giacomelli/SampleProject" SpecFlow %ERRORLEVEL%
 
 @echo Done!
 @pause
